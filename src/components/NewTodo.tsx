ef653f5e1d143e0c8dd4f6d4274f03ce7ee01bc4
mishkamag <m93.maglaperidze@gmail.com>
2022-10-04 13:19:50 +0400

    finished with context api

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,11 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
+import { TodosContext } from "../store/todos-context";
 import classes from "./NewTodo.module.css";
 
-const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
@@ -11,16 +14,16 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const enteredText = todoTextInputRef.current!.value;
 
     if (enteredText.trim().length === 0) {
-      // throm an error
+      // throw an error
       return;
     }
 
-    props.onAddTodo(enteredText);
+    todosCtx.addTodo(enteredText);
   };
 
   return (
     <form onSubmit={submitHandler} className={classes.form}>
-      <label htmlFor="text">Todo Text</label>
+      <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
